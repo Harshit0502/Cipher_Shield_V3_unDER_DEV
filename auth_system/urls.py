9b2c27b21ae82190c864ef38b4ceaf04592c426b
Harshit0502 <harshitmahour360@gmail.com>
2025-04-27 00:08:16 +0530

    Updation

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='signup'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('auth/private_key/', PrivateKeyRetrieveView.as_view(), name='get_private_key'),
+    path('private_key/<str:username>/', PrivateKeyRetrieveView.as_view(), name='private_key_retrieve'),
+
 ]
