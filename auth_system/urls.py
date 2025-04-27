bf711b0369221129bae149256035ec70a4217e6b
Harshit0502 <125523493+Harshit0502@users.noreply.github.com>
2025-04-27 11:41:18 +0530

    Update urls.py

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import UserSignupView, UserLoginView
-from .views import PrivateKeyRetrieveView
+from .views import UserSignupView, UserLoginView, PrivateKeyRetrieveView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='signup'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('auth/private_key/', PrivateKeyRetrieveView.as_view(), name='get_private_key'),
-    path('private_key/<str:username>/', PrivateKeyRetrieveView.as_view(), name='private_key_retrieve'),
-
+    path('private_key/', PrivateKeyRetrieveView.as_view(), name='private-key'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
